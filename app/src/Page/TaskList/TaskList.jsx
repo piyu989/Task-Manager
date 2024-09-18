0d0be579713b70a0a044fb788a79443240d4ada3
piyush <sab9861@gmail.com>
2024-09-18 22:39:42 +0530

    map error resolved

--- app/src/Page/TaskList/TaskList.jsx
+++ app/src/Page/TaskList/TaskList.jsx
@@ -1,40 +1,38 @@
-import React, { useEffect } from 'react'
-import TaskCard from '../Task/TaskCard/TaskCard'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect } from 'react';
+import TaskCard from '../Task/TaskCard/TaskCard';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchTasks, fetchUsersTask } from '../../ReduxToolKit/TaskSlice';
 import { useLocation } from 'react-router-dom';
 
 const TaskList = () => {
 
-  const dispatch=useDispatch();
-  const {task,auth}=useSelector(store=>store)
-  const location=useLocation();
-  const queryParams=new URLSearchParams(location.search);
-  const filterValue=queryParams.get("filter")
+  const dispatch = useDispatch();
+  const { task, auth } = useSelector(store => store);
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const filterValue = queryParams.get("filter");
 
-
-
-  useEffect(()=>{
-    if(auth.user?.role==="ROLE_ADMIN"){
-      dispatch(fetchTasks({status:filterValue}));
-    }else{
-      dispatch(fetchUsersTask({status:filterValue}));
+  useEffect(() => {
+    if (auth.user?.role === "ROLE_ADMIN") {
+      dispatch(fetchTasks({ status: filterValue }));
+    } else {
+      dispatch(fetchUsersTask({ status: filterValue }));
     }
-  },[filterValue]);
+  }, [filterValue, auth.user?.role, dispatch]);
 
-  console.log("tasks: ",task)
+  console.log("tasks: ", task);
+
+  const tasksToRender = auth.user?.role === "ROLE_ADMIN" ? task.tasks : task.userTask;
 
   return (
     <div className='w-[65vw]'>
-        <div className='space-y-6'>
-            {auth.user?.role==="ROLE_ADMIN"?task.tasks.map((item)=>(
-              <TaskCard item={item}/>)):task.userTask.map((item) => (
-                <TaskCard item={item}/>
-              ))
-            }
-        </div>
+      <div className='space-y-6'>
+        {tasksToRender?.map((item) => (
+          <TaskCard key={item.id} item={item} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
